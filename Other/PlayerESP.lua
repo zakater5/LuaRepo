bbe202c16d1d0eb3760e79001df90ab327654173
zakater <83347333+zakater5@users.noreply.github.com>
2022-08-02 01:36:07 +0200

    Update PlayerESP.lua

--- Other/PlayerESP.lua
+++ Other/PlayerESP.lua
@@ -1,163 +1,123 @@
-MobEsp = false
-MobLocations = {game:GetService("Workspace")} --add locations of the mobs in the workspace
-MobNames = {""} --add the names of the mobs
+-- made by rang#2415 or https://v3rmillion.net/member.php?action=profile&uid=1906262
+local Config = {
+    Box               = false,
+    BoxOutline        = false,
+    BoxColor          = Color3.fromRGB(255,255,255),
+    BoxOutlineColor   = Color3.fromRGB(0,0,0),
+    HealthBar         = false,
+    HealthBarSide     = "Left", -- Left,Bottom,Right
+    Names             = false,
+    NamesOutline      = false,
+    NamesColor        = Color3.fromRGB(255,255,255),
+    NamesOutlineColor = Color3.fromRGB(0,0,0),
+    NamesFont         = 2, -- 0,1,2,3
+    NamesSize         = 13
+}
 
-PlayerESP = true
+function CreateEsp(Player)
+    local Box,BoxOutline,Name,HealthBar,HealthBarOutline = Drawing.new("Square"),Drawing.new("Square"),Drawing.new("Text"),Drawing.new("Square"),Drawing.new("Square")
+    local Updater = game:GetService("RunService").RenderStepped:Connect(function()
+    if Player.Character ~= nil and Player.Character:FindFirstChild("Humanoid") ~= nil and Player.Character:FindFirstChild("HumanoidRootPart") ~= nil and Player.Character.Humanoid.Health > 0 and Player.Character:FindFirstChild("Head") ~= nil then
+            local Target2dPosition,IsVisible = workspace.CurrentCamera:WorldToViewportPoint(Player.Character.HumanoidRootPart.Position)
+            local scale_factor = 1 / (Target2dPosition.Z * math.tan(math.rad(workspace.CurrentCamera.FieldOfView * 0.5)) * 2) * 100
+            local width, height = math.floor(40 * scale_factor), math.floor(60 * scale_factor)
+            if Config.Box then
+                Box.Visible = IsVisible
+                Box.Color = Config.BoxColor
+                Box.Size = Vector2.new(width,height)
+                Box.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2)
+                Box.Thickness = 1
+                Box.ZIndex = 69
+                if Config.BoxOutline then
+                    BoxOutline.Visible = IsVisible
+                    BoxOutline.Color = Config.BoxOutlineColor
+                    BoxOutline.Size = Vector2.new(width,height)
+                    BoxOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2)
+                    BoxOutline.Thickness = 3
+                    BoxOutline.ZIndex = 1
+                else
+                    BoxOutline.Visible = false
+                end
+            else
+                Box.Visible = false
+                BoxOutline.Visible = false
+            end
+            if Config.Names then
+                Name.Visible = IsVisible
+                Name.Color = Config.NamesColor
+                Name.Text = Player.Name.." "..math.floor((workspace.CurrentCamera.CFrame.p - Player.Character.HumanoidRootPart.Position).magnitude).."m"
+                Name.Center = true
+                Name.Outline = Config.NamesOutline
+                Name.OutlineColor = Config.NamesOutlineColor
+                Name.Position = Vector2.new(Target2dPosition.X,Target2dPosition.Y - height * 0.5 + -15)
+                Name.Font = Config.NamesFont
+                Name.Size = Config.NamesSize
+            else
+                Name.Visible = false
+            end
+            if Config.HealthBar then
+                HealthBarOutline.Visible = IsVisible
+                HealthBarOutline.Color = Color3.fromRGB(0,0,0)
+                HealthBarOutline.Filled = true
+                HealthBarOutline.ZIndex = 1
+    
+                HealthBar.Visible = IsVisible
+                HealthBar.Color = Color3.fromRGB(255,0,0):lerp(Color3.fromRGB(0,255,0), Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth)
+                HealthBar.Thickness = 1
+                HealthBar.Filled = true
+                HealthBar.ZIndex = 69
+                if Config.HealthBarSide == "Left" then
+                    HealthBarOutline.Size = Vector2.new(2,height)
+                    HealthBarOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2) + Vector2.new(-3,0)
+                    
+                    HealthBar.Size = Vector2.new(1,-(HealthBarOutline.Size.Y - 2) * (Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth))
+                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1,-1 + HealthBarOutline.Size.Y)
+                elseif Config.HealthBarSide == "Bottom" then
+                    HealthBarOutline.Size = Vector2.new(width,3)
+                    HealthBarOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2) + Vector2.new(0,height + 2)
 
-plr = game.Players.LocalPlayer
-_G.on = false --set to false if you want to turn it off
-
-names = {}
-function isin(obj,tbl)
-	for a = 1,#tbl do
-		if obj == tbl[a] then
-			return true
-		end
-	end
-	return false
-end
-function test1(D)
-	local d = tonumber(string.sub(D,1,string.find(D,".",1,true) + 1))
-	return d
-end
-function Label(Part,Distance)
-	local MainName = Part.Parent.Name
-	local check = Part:FindFirstChild(MainName .. " Tracker")
-	if check then
-		destroyed = false
-		while not destroyed do
-			for i = 1,#names do
-				if names[i] == MainName then
-					table.remove(names,i)
-					destroyed = true
-					break
-				end
-			end
-			destroyed = true
-		end
-		check:Destroy()
-	end
-	local Gui = Instance.new("BillboardGui")
-	local Text = Instance.new("TextLabel")
-	if Distance ~= nil then
-		local assa,D = pcall(test1,Distance)
-		if D ~= nil and D then
-			D = tonumber(D)
-			table.insert(names,MainName)
-			Gui.Name = MainName.." Tracker"
-			Gui.Parent = Part
-			Gui.Adornee = Part
-			Gui.ExtentsOffsetWorldSpace = Vector3.new(0,3,0)
-			Gui.MaxDistance = 2500
-			Gui.Size = UDim2.new(0,200,0,50)
-			Gui.AlwaysOnTop = true
-			Text.Parent = Gui
-			Text.TextWrapped = true
-			Text.BackgroundTransparency = 1
-			Text.TextSize = 8
-			Text.Size = UDim2.new(0, 200, 0, 50)
-			Text.Font = Enum.Font.Legacy
-			if D ~= nil and D < 30 then
-				Text.TextColor3 = Color3.fromRGB(33, 231, 40)
-				Text.Text = Part.Parent.Name .. "\n Dist: " .. D
-			elseif D ~= nil and D < 100 then
-				Text.TextColor3 = Color3.fromRGB(228, 231, 34)
-				Text.Text = Part.Parent.Name .. "\n Dist: " .. D
-			elseif D ~= nil and D < 500 then
-				Text.TextColor3 = Color3.fromRGB(255, 140, 46)
-				Text.Text = Part.Parent.Name .. "\n Dist: " .. D
-			elseif D ~= nil and D < 2500 then
-				Text.TextColor3 = Color3.fromRGB(255, 0, 0)
-				Text.Text = Part.Parent.Name .. "\n Dist: " .. D
-			end
-		end
-	end
+                    HealthBar.Size = Vector2.new((HealthBarOutline.Size.X - 2) * (Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth),1)
+                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1,-1 + HealthBarOutline.Size.Y)
+                elseif Config.HealthBarSide == "Right" then
+                    HealthBarOutline.Size = Vector2.new(2,height)
+                    HealthBarOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2) + Vector2.new(width + 1,0)
+                    
+                    HealthBar.Size = Vector2.new(1,-(HealthBarOutline.Size.Y - 2) * (Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth))
+                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1,-1 + HealthBarOutline.Size.Y)
+                end
+            else
+                HealthBar.Visible = false
+                HealthBarOutline.Visible = false
+            end
+        else
+            Box.Visible = false
+            BoxOutline.Visible = false
+            Name.Visible = false
+            HealthBar.Visible = false
+            HealthBarOutline.Visible = false
+            if not Player then
+                Box:Remove()
+                BoxOutline:Remove()
+                Name:Remove()
+                HealthBar:Remove()
+                HealthBarOutline:Remove()
+                Updater:Disconnect()
+            end
+        end
+    end)
 end
 
-function Root()
-	plr = game.Players:FindFirstChild(game.Players.LocalPlayer.Name)
-	if plr then
-		local root = plr.Character:FindFirstChild("HumanoidRootPart")
-		if root then
-			return root
-		else
-			for i = 1,#names do
-				local check = game.Workspace:FindFirstChild(names[i] .. " Tracker",true)
-				if check then
-					table.remove(names,i)
-					check:Destroy()
-					break
-				end
-			end
-			game:GetService('RunService').Stepped:wait()
-			Root()
-		end
-	end
+for _,v in pairs(game:GetService("Players"):GetPlayers()) do
+   if v ~= game:GetService("Players").LocalPlayer then
+      CreateEsp(v)
+      v.CharacterAdded:Connect(CreateEsp(v))
+   end
 end
 
-while _G.on do
-	game:GetService('RunService').Stepped:wait()
-	plr = game.Players.LocalPlayer
-	if MobEsp then
-		for iter = 1,#MobLocations do
-			local check = MobLocations[iter]:FindFirstChildOfClass("Model")
-			local check2 = MobLocations[iter]:FindFirstChildOfClass("Part")
-			if check or check2 then
-				for i,v in pairs(MobLocations[iter]:GetChildren()) do
-					if v.ClassName == "Model" or v.ClassName == "Part" and isin(v.Name,MobNames) == true then
-						local placed = false
-						for a,b in pairs(v:GetChildren()) do
-							if b.Name == "Head" and b.ClassName == "Part" and not placed and isin(v.Name,MobNames) == true then
-								placed = true
-								Label(b,(Root().Position - b.Position).Magnitude)
-							elseif b.Name == "UpperTorso" and b.ClassName == "Part" and not placed and isin(v.Name,MobNames) == true then
-								placed = true
-								Label(b,(Root().Position - b.Position).Magnitude)
-							elseif b.Name == "Torso" and b.ClassName == "Part" and not placed and isin(v.Name,MobNames) == true then
-								placed = true
-								Label(b,(Root().Position - b.Position).Magnitude)
-							elseif b.ClassName == "Part" and not placed and isin(v.Name,MobNames) == true then
-								placed = true
-								repeat wait() until game.Players.LocalPlayer.Character ~= nil
-								Label(b,(Root().Position - b.Position).Magnitude)
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-	if PlayerESP then
-		for z,d in pairs(game.Players:GetChildren()) do
-			if d.Character ~= nil and d ~= plr then
-				local prt = d.Character:FindFirstChildOfClass("Part")
-				if prt then
-					local placed = false
-					for j,g in pairs(d.Character:GetChildren()) do
-						if g.Name == "Head" and not placed then
-							placed = true
-							Label(g,(Root().Position - g.Position).Magnitude)
-						elseif g.Name == "UpperTorso" and not placed then
-							placed = true
-							Label(g,(Root().Position - g.Position).Magnitude)
-						elseif g.Name == "Torso" and not placed then
-							placed = true
-							Label(g,(Root().Position - g.Position).Magnitude)
-						end
-					end
-				end
-			end
-		end
-	end
-end
-while #names > 0 do
-	game:GetService('RunService').Stepped:wait()
-	for i = 1,#names do
-		local check = game.Workspace:FindFirstChild(names[i] .. " Tracker",true)
-		if check then
-			table.remove(names,i)
-			check:Destroy()
-			break
-		end
-	end
-end
+game:GetService("Players").PlayerAdded:Connect(function(v)
+   if v ~= game:GetService("Players").LocalPlayer then
+      CreateEsp(v)
+      v.CharacterAdded:Connect(CreateEsp(v))
+   end
+end)
+return Config
